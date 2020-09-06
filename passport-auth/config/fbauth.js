3609c6d1c6baf061ae00f9da212b2c8f7fef0f99
uranjan445 <58565275+uranjan445@users.noreply.github.com>
2020-09-06 09:59:41 +0530

    Revert "blog website"
    This reverts commit c0bd527abe20e1fdd765ed2aced87df7fcce4848.

--- passport-auth/config/fbauth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	'facebookAuth' : {
-		'clientID': '2768407126780205',
-		'clientSecret': '37315004e7445bd37603e0e9509f342f',
-		'callbackURL': 'http://localhost:3000/users/auth/facebook/callback'
-	}
-}
